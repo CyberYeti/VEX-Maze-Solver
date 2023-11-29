f66e6ff337560a3b8baaca9171199264ff038bef
CyberYeti <yitmengchin@gmail.com>
2023-11-28 19:09:35 -0800

    Added basic functions for movement
    Added Functions for moving the motors as well as getting the x and y position in mm.
    
    The movement functions for the x and y axis make each axis move at the same speed if the same speed value is passed in despite different gear ratios.
    EX: Even if running the x axis motors at max speed normally moves the axis faster than running the y axis motor at top speed, when running it through the movex() and movey() functions, the top speed of the x axis will be slowed in order to match that of the y axis.

--- Code/VEX 5 Code/MazeSolver.py
+++ Code/VEX 5 Code/MazeSolver.py
@@ -8,7 +8,7 @@ brain=Brain()
 # Robot configuration code
 yMotor = Motor(Ports.PORT11, GearSetting.RATIO_18_1, False)
 xMotor1 = Motor(Ports.PORT9, GearSetting.RATIO_18_1, True)
-xMotor2 = Motor(Ports.PORT10, GearSetting.RATIO_18_1, False)
+xMotor2 = Motor(Ports.PORT21, GearSetting.RATIO_18_1, False)
 
 
 # wait for rotation sensor to fully initialize
@@ -38,24 +38,107 @@ print("\033[2J")
 # Library imports
 import time
 
+#Variables
+waitDelay = 0.5
+updateDelay = 0.05
+
+#Degrees to mm
+chainLinkLen = 25.5/25
+dtmx = 12*chainLinkLen/360
+dtmy = 6*chainLinkLen/360
+maxdtm = max(abs(dtmx), abs(dtmy))
+
+#Velocity Multipliers
+#This is for making the speed of the x and y axis the same despite different gear ratios
+vmx = abs(dtmy/maxdtm)
+vmy = abs(dtmx/maxdtm)
+
 #Motor Setup
-# yMotor.spin(REVERSE)
-xMotor1.spin(REVERSE)
-xMotor2.spin(REVERSE)
+yMotor.spin(FORWARD)
+xMotor1.spin(FORWARD)
+xMotor2.spin(FORWARD)
 
-velo = 50;
-def MoveX(velocity):
-    xMotor1.set_velocity(-velocity, PERCENT)
-    xMotor2.set_velocity(-velocity, PERCENT)
+yMotor.set_velocity(0, PERCENT)
+xMotor1.set_velocity(0, PERCENT)
+xMotor2.set_velocity(0, PERCENT)
 
-MoveX(velo)
-while True:
-    brain.screen.clear_screen()
-    brain.screen.set_cursor(1, 1)
-    brain.screen.print(xMotor1.current(CurrentUnits.AMP))
-    if(xMotor1.current(CurrentUnits.AMP) > 1.25):
-        velo *= -1
-        MoveX(velo)
-    time.sleep(0.05)
+velo = 75;
+def movex(velocity):
+    xMotor1.set_velocity(velocity*vmx, PERCENT)
+    xMotor2.set_velocity(velocity*vmx, PERCENT)
+
+def movey(velocity):
+    yMotor.set_velocity(velocity*vmy, PERCENT)
 
 
+def getx():
+    return xMotor1.position(DEGREES)*dtmx
+
+def gety():
+    return yMotor.position(DEGREES)*dtmy
+
+# Homing Sequence
+movex(-velo)
+time.sleep(waitDelay)
+while xMotor1.current(CurrentUnits.AMP) < 2:
+    time.sleep(updateDelay)
+movex(0)
+xMotor1.set_position(0,DEGREES)
+xMotor2.set_position(0,DEGREES)
+
+movey(-velo)
+time.sleep(waitDelay)
+while yMotor.current(CurrentUnits.AMP) < 1:
+    time.sleep(updateDelay)
+movey(0)
+yMotor.set_position(0,DEGREES)
+
+#Find Bounds
+movex(velo)
+time.sleep(waitDelay)
+while xMotor1.current(CurrentUnits.AMP) < 2:
+    pass
+movex(0)
+
+movey(velo)
+time.sleep(waitDelay)
+while yMotor.current(CurrentUnits.AMP) < 1:
+    pass
+movey(0)
+
+brain.screen.print("x:", getx(), "   y:", gety())
+
+#Random Movement
+movex(-velo)
+time.sleep(waitDelay)
+while xMotor1.current(CurrentUnits.AMP) < 2:
+    time.sleep(updateDelay)
+movex(0)
+xMotor1.set_position(0,DEGREES)
+xMotor2.set_position(0,DEGREES)
+
+movey(-velo)
+time.sleep(waitDelay)
+while yMotor.current(CurrentUnits.AMP) < 1:
+    time.sleep(updateDelay)
+movey(0)
+yMotor.set_position(0,DEGREES)
+
+movex(velo)
+movey(velo)
+time.sleep(waitDelay)
+while (xMotor1.current(CurrentUnits.AMP) < 2) and (yMotor.current(CurrentUnits.AMP) < 1):
+    time.sleep(updateDelay)
+movex(0)
+movey(0)
+
+# # Collision Detection Test
+# movex(velo)
+# while True:
+#     brain.screen.clear_screen()
+#     brain.screen.set_cursor(1, 1)
+#     brain.screen.print(xMotor1.current(CurrentUnits.AMP))
+#     if(xMotor1.current(CurrentUnits.AMP) > 1.25):
+#         velo *= -1
+#         movex(velo)
+#     time.sleep(0.05)
